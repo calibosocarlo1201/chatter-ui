3ff6232a33759f89c563c6315a2a1b8faf067104
calibosocarlo1201 <calibosocarlo1201@gmail.com>
2025-06-07 21:02:39 +0800

    Created useLogin Hook

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,10 +1,13 @@
 import Auth from './Auth'
 import { Link as RouterLink } from 'react-router-dom'
 import { Link as MUILink} from '@mui/material'
+import { useLogin } from '../../hooks/useLogin'
 
 const Login = () => {
+  const { login, error } = useLogin();
+
   return (
-    <Auth submitLabel="Login" onSubmit={async () => {}} >
+    <Auth submitLabel="Login" onSubmit={(request) => login(request)} >
         {/* <Link to='/signup' style={{alignSelf: 'center'}}>
             <MUILink>Sign Up</MUILink>
         </Link> */}
