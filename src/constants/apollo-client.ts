99a48c0493f14a15332c05f1faf6f25851eee9eb
calibosocarlo1201 <calibosocarlo1201@gmail.com>
2025-06-08 14:43:18 +0800

    Added Redirect Unauthenticated Users/Request

--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -1,11 +1,37 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
 import { API_URL } from './urls'
+import excludedRoutes from './excluded-routes'
+import router from '../components/Routes'
 
-console.log('API_URL:', API_URL);
+//const logoutLink = onError((error) => {
+    // if(error.graphQLErrors?.length && (error.graphQLErrors[0].extensions.originalError as any).statusCode === 401){
+    //     if(!excludedRoutes.includes.(window.location.pathname)){
+    //         router.navigate('/login')
+    //     }
+    // }
+//});
+
+const logoutLink = onError((error) => {
+
+    function hasStatusCode(err: unknown): err is { statusCode: number } {
+        return typeof err === 'object' && err !== null && 'statusCode' in err && typeof (err as any).statusCode === 'number';
+    }
+
+    const originalError = error.graphQLErrors?.[0]?.extensions?.originalError;
+
+    if (hasStatusCode(originalError) && originalError.statusCode === 401) {
+        if (!excludedRoutes.includes(window.location.pathname)) {
+            router.navigate('/login');
+        }
+    }
+});
+
+const httpLink = new HttpLink({uri: `${API_URL}/graphql`})
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: `${API_URL}/graphql`
-})
+    link: logoutLink.concat(httpLink)
+});
 
 export default client
